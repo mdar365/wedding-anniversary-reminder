29f359e43f886cf1666e79dff738c3fa5533182e
mdar365 <68842181+mdar365@users.noreply.github.com>
2023-10-21 06:36:28 +1300

    Create classes for records

---
+++ records.py
@@ -0,0 +1,13 @@
+class CoupleRecord:
+    def __init__(self, id, wedding_date):
+        self.id = id
+        self.wedding_date = wedding_date
+
+class AnniversaryReminderRecord:
+    def __init__(self, id, next_wedding_anniversary):
+        self.id = id
+        self.next_wedding_anniversary = next_wedding_anniversary
+
+special_case_anniversary_configs =
+
+def generate_reminder_records(current_date, records):
