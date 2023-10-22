3a51e5601225850aee8e9e534caf52dce7dcf519
mdar365 <68842181+mdar365@users.noreply.github.com>
2023-10-22 17:03:13 +1300

    Remove unnecessary test

--- WeddingAnniversaryHandlerTest.kt
+++ WeddingAnniversaryHandlerTest.kt
@@ -170,37 +170,4 @@ class WeddingAnniversaryHandlerTest {
         )
         assertEquals(listOf(expectedReminderRecord1, expectedReminderRecord2), reminderRecords)
     }
-
-    @Test
-    fun testDefaultReminderWeeksMultipleCouples() {
-        val config = AnniversaryConfig(defaultReminderWeeks = 1, specialAnniversaryConfig = listOf())
-        val weddingAnniversaryHandler = WeddingAnniversaryHandler(currentDateIso, config)
-
-        val coupleId1 = "1234"
-        val weddingDate1 = Date(122, 9, 28, 6, 30)
-        val weddingDateIso1 = weddingDate1.toInstant().toString()
-        val coupleRecord1 = CoupleRecord(id = coupleId1, weddingDateIso1)
-
-        val coupleId2 = "4321"
-        val weddingDate2 = Date(121, 9, 27, 6, 30)
-        val weddingDateIso2 = weddingDate2.toInstant().toString()
-        val coupleRecord2 = CoupleRecord(id = coupleId2, weddingDateIso2)
-
-        val reminderRecords = weddingAnniversaryHandler.generateReminderRecords(listOf(coupleRecord1, coupleRecord2))
-
-        val nextAnniversaryDate1 = weddingDate1.clone() as Date
-        nextAnniversaryDate1.year = weddingDate1.year + 1
-        val expectedReminderRecord1 = WeddingAnniversaryReminderRecord(
-            coupleId1,
-            nextAnniversaryDate1.toInstant().toString()
-        )
-
-        val nextAnniversaryDate2 = weddingDate2.clone() as Date
-        nextAnniversaryDate2.year = weddingDate2.year + 2
-        val expectedReminderRecord2 = WeddingAnniversaryReminderRecord(
-            coupleId2,
-            nextAnniversaryDate2.toInstant().toString()
-        )
-        assertEquals(listOf(expectedReminderRecord1, expectedReminderRecord2), reminderRecords)
-    }
 }
